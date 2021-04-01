8cc358685ea6ea2da0632dd852a9b05ce1d45d28
blinov.nstu <blinov.nstu@gmail.com>
2021-04-01 13:15:51 +0700

    добавил проверку действующих proxy

--- proxy_magic.py
+++ proxy_magic.py
@@ -1,46 +1,64 @@
-from proxy_checker import ProxyChecker
+# -*- coding: utf-8 -*-
 from proxyscrape import create_collector
+import requests as r
 import time
 
 
-def get_proxies(region_codes: tuple = ('ru'), anonymous: bool = True, type: str = 'https', all: bool = False) -> list:
+# TODO: комментарии, ускорить
+def get_valid_proxies(iso_codes: tuple = ('ru'), anonymous: bool = True,
+                type_proxy: str = 'https', all: bool = False, number_of_checks:int = 100,
+                check_timeout: int = 10) -> list:
     collector = create_collector(
         name='default_collector',
-        resource_types=type,
+        resource_types=type_proxy,
         refresh_interval=3600,
-        resources=None
     )
-    collector.apply_filter(
-        {
-            'code': region_codes,
-            'anonymous': anonymous,
-            'type': type
-        })
-    if not all:
-        p = collector.get_proxy()
-        return [f'{p.host}:{p.port}']
-    proxies = []
+    filter_dict = {
+        'anonymous': anonymous,
+        'type': type_proxy
+    }
+    if iso_codes != ():
+        filter_dict['code'] = iso_codes
+    collector.apply_filter(filter_dict)
+    proxies, valid_proxies, i = [], [], 0
     while True:
         p = collector.get_proxy()
-        if p:
-            if p not in proxies:
-                proxies.append(f'{p.host}:{p.port}')
+        print(p)
+        if p and i < number_of_checks:
+            p_str =  f'{p.host}:{p.port}'
+            if p_str not in proxies:
+                print('-', p_str)
+                proxies.append(p_str)
+                try:
+                    if type_proxy == 'http':
+                        req = r.get('http://api.ipify.org?fromat=json', proxies={'http': p_str}, timeout=check_timeout)
+                    else:
+                        req = r.get('https://api.ipify.org?fromat=json', proxies={'https': p_str}, timeout=check_timeout)
+                    if req.text == p.host:
+                        print('--', p_str)
+                        if not all:
+                            return [p_str]
+                        valid_proxies.append(p_str)
+                        collector.remove_proxy(p)
+                except:
+                    collector.remove_proxy(p)
+                    continue
+            else:
                 collector.remove_proxy(p)
         else:
             break
-    return proxies
-
-
-def filter_bad_proxies(proxies: list) -> list:
-    checker = ProxyChecker()
-    return [prx for prx in proxies if checker.check_proxy(proxy=prx, check_country=False, check_address=False) != False]
+        i += 1
+    return valid_proxies
 
 
 def main():
-    l1 = get_proxies(all=True)
-    print(l1)
-    l2 = filter_bad_proxies(l1)
-    print(l2)
+    l = get_valid_proxies(
+        iso_codes=(),
+        type_proxy='http',
+        all=True,
+        number_of_checks=30,
+        check_timeout=3)
+    print(l)
     
 
 if __name__ == '__main__':
